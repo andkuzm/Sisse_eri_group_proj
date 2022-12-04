c19476f5c7df6a2237b910655028561c88b50079
ArianaJogi <arianajogi@gmail.com>
2022-12-04 20:31:42 +0200

    C&Bo changes

--- General JS.js
+++ General JS.js
@@ -1,4 +1,4 @@
-document.body.addEventListener("pointermove", (e)=>{
+document.body.addEventListener("pointermove", (e)=>{  //https://codepen.io/MarkBoots/pen/LYjvEJj
     const { currentTarget: el, clientX: x, clientY: y } = e;
     const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
     el.style.setProperty('--posX',  x-l-w/2);
