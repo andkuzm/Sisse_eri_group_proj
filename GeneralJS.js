6a5f08bc4004af3b5eff6800b715e8f759fdfdac
ArianaJogi <arianajogi@gmail.com>
2022-12-08 00:55:06 +0200

    small fixes

--- GeneralJS.js
+++ GeneralJS.js
@@ -1,6 +1,6 @@
-document.body.addEventListener("pointermove", (e)=>{  //https://codepen.io/MarkBoots/pen/LYjvEJj
-    const { currentTarget: el, clientX: x, clientY: y } = e;
-    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
-    el.style.setProperty('--posX',  x-l-w/2);
-    el.style.setProperty('--posY',  y-t-h/2);
-  })
+document.body.addEventListener("pointermove", (e)=>{  //tegu, kui kursoor liikub
+  const { currentTarget: el, clientX: x, clientY: y } = e; //praeguse kursoori positsioon.
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect(); //lehe paraametrid
+  el.style.setProperty('--posX',  x-l-w/2); //tegu, kursoori praeguse positsiooni tõttu
+  el.style.setProperty('--posY',  y-t-h/2); //tegu, kursoori praeguse positsiooni tõttu
+})//allikas https://codepen.io/MarkBoots/pen/LYjvEJj?fbclid=IwAR2i7FIU0Rk5wM5PVR12g8b6LsGeEdPBc-NUE2AS6a2NmsZmgzkWa6KmMTY
